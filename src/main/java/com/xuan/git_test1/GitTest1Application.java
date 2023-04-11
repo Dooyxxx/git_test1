d79d07abc7d3fc0899e891a6e88133eb569fb086
Dooyxxx <2353841267@qq.com>
2023-04-11 10:02:11 +0800

    First submit

---
+++ src/main/java/com/xuan/git_test1/GitTest1Application.java
@@ -0,0 +1,13 @@
+package com.xuan.git_test1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitTest1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GitTest1Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/xuan/git_test1/GitTest1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xuan.git_test1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitTest1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
